f8ad6aef894a26ff89cfaca6130105bedfdaaa27
Er4ikk <82507949+Er4ikk@users.noreply.github.com>
2022-01-09 17:47:01 +0100

    defining_joystick_axis

--- joystick_prova.ino
+++ joystick_prova.ino
@@ -3,6 +3,16 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+int joystick_right_x=A0;
+int joystick_right_y=A1;
+int value_right_x=0;
+int value_right_y=0;
+
+int joystick_left_x=A2;
+int joystick_left_y=A3;
+int value_left_x=0;
+int value_left_y=0;
+
 void setup(){}
 
 void loop(){}
